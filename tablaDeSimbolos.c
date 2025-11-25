21e2e01e27230d32735bb728c1f261b998843b3a
LeyreLeiza <149714105+LeyreLeiza@users.noreply.github.com>
2025-11-25 16:47:56 +0100

    Update tablaDeSimbolos.c

--- tablaDeSimbolos.c
+++ tablaDeSimbolos.c
@@ -4,11 +4,11 @@
 #include "tablaDeSimbolos.h"
 
 
-tablaDeSimbolos nuevaTablaDeSimbolos() {
+TablaDeSimbolos nuevaTablaDeSimbolos() {
     return (Celda *) NULL;
 }
 
-bool insertaSimbolo(tablaDeSimbolos * ptc, char * pnombre, LiteralT v){
+bool insertaSimbolo(TablaDeSimbolos * ptc, char * pnombre, LiteralT v){
     Celda * c;
     c = *ptc;
     while (c != NULL && strcmp(pnombre, c->nombre))
@@ -27,6 +27,38 @@ bool insertaSimbolo(tablaDeSimbolos * ptc, char * pnombre, LiteralT v){
     }
 }
 
+bool insertaSimboloSinLiteral(TablaDeSimbolos * ptc, char * pnombre, char * v){
+    Celda * c;
+    c = *ptc;
+    while (c != NULL && strcmp(pnombre, c->nombre))
+        c = c->sig;
+    if (c != NULL)
+        printf("Ya existe una constante llamada %s\n", pnombre);
+    else {
+        if ((c = (Celda *) malloc(sizeof(Celda))) == NULL) {
+            printf("No hay memoria para una celda de la tabla de simbolos");
+            exit(1);
+        }
+        LiteralT aux;
+        //esto no sirve hay que cambiarlo pero para ver si lo demas funciona
+        if(strcmp(v, "entero") == 0){
+            aux = nuevoLiteralEntero(0);
+        }else if(strcmp(v, "real") == 0){
+            aux = nuevoLiteralReal(0.0);
+        }else if(strcmp(v, "booleano") == 0){
+            aux = nuevoLiteralBooleano(false);
+        }else if(strcmp(v, "cadena") == 0){
+            aux = nuevoLiteralCadena("");
+        }else if(strcmp(v, "caracter") == 0){
+            aux = nuevoLiteralCaracter('a');
+        }
+        c->nombre = pnombre;
+        c->valor = aux;
+        c->sig = *ptc;
+        *ptc = c;
+    }
+}
+
 void imprimeTablaDeSimbolos(TablaDeSimbolos tc){
     Celda * aux = tc;
     if (tc = NULL)
@@ -40,15 +72,4 @@ void imprimeTablaDeSimbolos(TablaDeSimbolos tc){
             aux = aux ->sig;
         }
     }
-}
-
-bool buscarSimbolo(TablaDeSimbolos tc, char *pnombre) {
-    Celda *c = tc;
-    while (c != NULL) {
-        if (strcmp(c->nombre, pnombre) == 0) {
-            return true;
-        }
-        c = c->sig;
-    }
-    return false;
 }
