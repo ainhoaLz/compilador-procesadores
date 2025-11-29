a621deb0149d502ff59878d428f15eee00f58dbf
LeyreLeiza <149714105+LeyreLeiza@users.noreply.github.com>
2025-11-29 09:15:07 +0100

    Update literal.c

--- literal.c
+++ literal.c
@@ -1,12 +1,14 @@
-#include "literal.h" 
+#include "literal.h"
 #include "nombresDeTipos.h" //Para poder usar nobres de tipo
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 LiteralT nuevoLiteralBooleano(ValorBooleanoT valor){
     LiteralT l;
     l.tipoDelValor = BOOLEANO;
+    l.tieneValor = true;
     l.valor.valorBooleano = valor;
     return l;
 }
@@ -14,6 +16,7 @@ LiteralT nuevoLiteralBooleano(ValorBooleanoT valor){
 LiteralT nuevoLiteralCadena(char * valor){
     LiteralT l;
     l.tipoDelValor = CADENA;
+    l.tieneValor = true;
     if ((l.valor.valorCadena = strdup(valor)) == NULL){
         printf("\t\t Literal.c: No hasy memoria para crear la constante %s\n", valor);
         exit(1);
@@ -24,6 +27,7 @@ LiteralT nuevoLiteralCadena(char * valor){
 LiteralT nuevoLiteralCaracter(char valor){
     LiteralT l;
     l.tipoDelValor = CARACTER;
+    l.tieneValor = true;
     l.valor.valorCaracter = valor;
     return l;
 }
@@ -31,6 +35,7 @@ LiteralT nuevoLiteralCaracter(char valor){
 LiteralT nuevoLiteralEntero(int valor){
     LiteralT l;
     l.tipoDelValor = ENTERO;
+    l.tieneValor = true;
     l.valor.valorEntero = valor;
     return l;
 }
@@ -38,32 +43,51 @@ LiteralT nuevoLiteralEntero(int valor){
 LiteralT nuevoLiteralReal(float valor){
     LiteralT l;
     l.tipoDelValor = REAL;
+    l.tieneValor = true;
     l.valor.valorReal = valor;
     return l;
 }
 
 void escribeLiteral(LiteralT l){
     switch (l.tipoDelValor) {
-        case BOOLEANO: 
+        case BOOLEANO:
             printf("Booleano\t");
-            if (l.valor.valorBooleano == VERDADERO)
-                printf("Verdadero\n");
-            else
-                printf("Falso\n");
+            if(l.tieneValor){
+                printf("%s", l.valor.valorBooleano == VERDADERO ? "Verdadero" : "Falso");
+            }
             break;
-        case CADENA: 
-            printf("Cadena\t\t%s\n",l.valor.valorCadena);
+        case CADENA:
+            printf("Cadena\t\t");
+            if(l.tieneValor){
+                printf("%s", l.valor.valorCadena);
+            }
             break;
-        case CARACTER: 
-            printf("Caracter\t%c\n",l.valor.valorCaracter);
-            break; 
-        case ENTERO: 
-            printf("Entero\t\t%d\n",l.valor.valorEntero);
+        case CARACTER:
+            printf("Caracter\t");
+            if(l.tieneValor){
+                printf("%c", l.valor.valorCaracter);
+            }
             break;
-        case REAL: 
-            printf("Real\t\t%lf\n",l.valor.valorReal);
+        case ENTERO:
+            printf("Entero\t\t");
+            if(l.tieneValor){
+                printf("%d", l.valor.valorEntero);
+            }
+            break;
+        case REAL:
+            printf("Real\t\t");
+            if(l.tieneValor){
+                printf("%f", l.valor.valorReal);
+            }
             break;
     }
+    printf("\n");
 }
 
 
+LiteralT nuevoLiteralSinValor(NombreDeTipoT tipo){
+    LiteralT l;
+    l.tipoDelValor = tipo;
+    l.tieneValor = false;
+    return l;
+}
