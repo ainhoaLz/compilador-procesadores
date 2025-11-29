5f4e27e7843cafe2561dc031fe0da3bc783db1ea
LeyreLeiza <149714105+LeyreLeiza@users.noreply.github.com>
2025-11-29 09:14:00 +0100

    Update tablaDeSimbolos.c

--- tablaDeSimbolos.c
+++ tablaDeSimbolos.c
@@ -27,41 +27,9 @@ bool insertaSimbolo(TablaDeSimbolos * ptc, char * pnombre, LiteralT v){
     }
 }
 
-bool insertaSimboloSinLiteral(TablaDeSimbolos * ptc, char * pnombre, char * v){
-    Celda * c;
-    c = *ptc;
-    while (c != NULL && strcmp(pnombre, c->nombre))
-        c = c->sig;
-    if (c != NULL)
-        printf("Ya existe una constante llamada %s\n", pnombre);
-    else {
-        if ((c = (Celda *) malloc(sizeof(Celda))) == NULL) {
-            printf("No hay memoria para una celda de la tabla de simbolos");
-            exit(1);
-        }
-        LiteralT aux;
-        //esto no sirve hay que cambiarlo pero para ver si lo demas funciona
-        if(strcmp(v, "entero") == 0){
-            aux = nuevoLiteralEntero(0);
-        }else if(strcmp(v, "real") == 0){
-            aux = nuevoLiteralReal(0.0);
-        }else if(strcmp(v, "booleano") == 0){
-            aux = nuevoLiteralBooleano(false);
-        }else if(strcmp(v, "cadena") == 0){
-            aux = nuevoLiteralCadena("");
-        }else if(strcmp(v, "caracter") == 0){
-            aux = nuevoLiteralCaracter('a');
-        }
-        c->nombre = pnombre;
-        c->valor = aux;
-        c->sig = *ptc;
-        *ptc = c;
-    }
-}
-
 void imprimeTablaDeSimbolos(TablaDeSimbolos tc){
     Celda * aux = tc;
-    if (tc = NULL)
+    if (tc == NULL)
         printf("La tabla de simbolos está vacía\n");
     else {
         printf("El contenido de la tabla de simbolos es.\n");
@@ -72,4 +40,21 @@ void imprimeTablaDeSimbolos(TablaDeSimbolos tc){
             aux = aux ->sig;
         }
     }
+}
+
+bool actualizarValor(TablaDeSimbolos *ptc, char *nombre, LiteralT nuevoValor){
+    Celda *c = *ptc;
+
+    while (c != NULL && strcmp(c->nombre, nombre) != 0){
+        c = c->sig;
+    }
+
+    if (c == NULL) {
+        printf("Error: no existe la variable %s\n", nombre);
+        return false;
+    }
+
+    c->valor = nuevoValor;
+    c->valor.tieneValor = true;
+    return true;
 }
